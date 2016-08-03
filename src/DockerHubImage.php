6fb1b3aedf25960d217fb9935ec6f6c817b1acd3
Sébastien Santoro <dereckson@espace-win.org>
2016-08-03 19:44:08 +0000

    Allow to get a TriggerBuild instance from DockerHubImage
    Summary:
    Goal is to allow fluent pattern:
    
    ```lang=php
    $image = new DockerHubImage("acme", "foo");
    $payload = $image
        ->getTriggerBuild('0000')
        ->getPayloadForDockerTag('7.1')
    ```
    
    Ref T965.
    
    Test Plan: New unit test.
    
    Reviewers: dereckson
    
    Tags: #tdd
    
    Maniphest Tasks: T965
    
    Differential Revision: https://devcentral.nasqueron.org/D598

--- src/DockerHubImage.php
+++ src/DockerHubImage.php
@@ -2,6 +2,8 @@
 
 namespace Keruald\DockerHub;
 
+use Keruald\DockerHub\Build\TriggerBuild;
+
 /**
  * Represents a DockerHub image.
  */
@@ -9,6 +11,17 @@ class DockerHubImage extends DockerImage {
 
     public function getRegistryUrl () {
         return "https://registry.hub.docker.com/u/$this->user/$this->image";
+    }
+
+    /**
+     * Gets a trigger build for this image
+     * to be able to prepare a build trigger payload.
+     *
+     * @param string $token The token to authentify the build request
+     * @return Keruald\DockerHub\Build\TriggerBuild
+     */
+    public function getTriggerBuild ($token) {
+        return new TriggerBuild($this, $token);
     }
 
 }
--- tests/DockerHubImageTest.php
+++ tests/DockerHubImageTest.php
@@ -3,14 +3,30 @@
 namespace Keruald\DockerHub\Tests;
 
 use Keruald\DockerHub\DockerHubImage;
+use Keruald\DockerHub\Build\TriggerBuild;
 
 class DockerHubImageTest extends \PHPUnit_Framework_TestCase {
 
+    /**
+     * @var Keruald\DockerHub\DockerHubImage
+     */
+    private $image;
+
+    public function setUp () {
+        $this->image = new DockerHubImage("acme", "foo");
+    }
+
     public function testGetRegistryUrl () {
-        $image = new DockerHubImage("acme", "foo");
         $this->assertSame(
             "https://registry.hub.docker.com/u/acme/foo",
-            $image->getRegistryUrl()
+            $this->image->getRegistryUrl()
+        );
+    }
+
+    public function testGetTriggerBuild () {
+        $this->assertInstanceOf(
+            TriggerBuild::class,
+            $this->image->getTriggerBuild('0000')
         );
     }
 
